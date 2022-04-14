edd218b55e17de3046bb915a177a5046760923a7
despratefor <taozm163@163.com>
2022-04-14 10:20:46 +0800

    已完成

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-
+#include "listNode.h"
 
 using namespace std;
 
 //全局变量
-struct ListNode {
-	     int val;
-	     struct ListNode *next;
-};
+// struct ListNode {
+// 	     int val;
+// 	     struct ListNode *next;
+// };
+ typedef link ListNode ;
 
 //题目函数
 /**
@@ -19,40 +20,77 @@ struct ListNode {
  */
 
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+link* addTwoNumbers( link* l1, link* l2){
 	int overten = 0;
+	link* l1head = l1;
 
-	while(l1!=NULL && l2!=NULL)
+	while(l1!=NULL || l2!=NULL)
 	{
-		if ((l1->val + l2->val + overten) <10)
+		if(l1->next==NULL && l2->next!=NULL)//如果L1链表较短，则以0补齐
 		{
-			l1->val = l1->val + l2->val + overten;
+			link* supply = (link*) malloc(sizeof(link));
+			supply->elem = 0;
+			supply->next = NULL;
+			l1->next = supply;
+		}
+		if(l2->next==NULL && l1->next!=NULL)//如果L2链表较短，则以0补齐
+		{
+			link* supply = (link*) malloc(sizeof(link));
+			supply->elem = 0;
+			supply->next = NULL;
+			l2->next = supply;
+		}
+
+		if ((l1->elem + l2->elem + overten) <10)	//两条链表上的数相加
+		{
+			l1->elem = l1->elem + l2->elem + overten;
 			overten = 0;
 		} 
 		else
 		{
-			l1->val = l1->val + l2->val - 10 + overten; //大于十，进一位
+			l1->elem = l1->elem + l2->elem - 10 + overten; //大于十，进一位
 			overten = 1;
+		}
+
+		if(l1->next==NULL && l2->next==NULL && overten==1)//如果最高位有进位
+		{
+			link* supply = (link*) malloc(sizeof(link));
+			supply->elem = 1;
+			supply->next = NULL;
+			l1->next = supply;
+			overten = 0;
+			break;
 		}
 		l1 = l1->next;
 		l2 = l2->next;
 	}
 
-	if(l2==NULL && l1!=NULL)
-	{
-
-	}
-
-	return l1;
+	return l1head;
 }
 
 //测试构建
 
 int main(void)
 {
+	link *p1=initLink();
+	addElem(p1,9);
+	addElem(p1,9);
+	addElem(p1,9);
+	addElem(p1,9);
+	addElem(p1,9);
+	addElem(p1,9);
+	addElem(p1,9);
 
+	link *p2=initLink();
+	addElem(p2,9);
+	addElem(p2,9);
+	addElem(p2,9);
+	addElem(p2,9);
 
+	link* rst = initLink();
+	rst = addTwoNumbers(p1,p2);
 
+	return 0;
 }
 
 
